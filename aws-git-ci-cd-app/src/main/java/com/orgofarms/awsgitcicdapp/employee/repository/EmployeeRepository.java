7be37339a67acceaec036716f416fccee1e6fe47
codecoder005 <85489242+codecoder005@users.noreply.github.com>
2022-02-08 11:20:08 +0530

    Delete aws-git-ci-cd-app directory

--- aws-git-ci-cd-app/src/main/java/com/orgofarms/awsgitcicdapp/employee/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.orgofarms.awsgitcicdapp.employee.repository;
-
-import com.orgofarms.awsgitcicdapp.employee.jpa.entity.EmployeeEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<EmployeeEntity, Integer> {
-}
