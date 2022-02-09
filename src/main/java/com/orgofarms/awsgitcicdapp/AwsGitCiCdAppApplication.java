1b9086a81a4b8293983279768cd75bdbf12bcc13
Ramakrishna Janapureddy <developer.raakhi005@gmail.com>
2022-02-09 13:23:37 +0530

    packaging changed to war to deploy into tomcat

--- src/main/java/com/orgofarms/awsgitcicdapp/AwsGitCiCdAppApplication.java
+++ src/main/java/com/orgofarms/awsgitcicdapp/AwsGitCiCdAppApplication.java
@@ -1,13 +1,30 @@
 package com.orgofarms.awsgitcicdapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-public class AwsGitCiCdAppApplication {
+public class AwsGitCiCdAppApplication extends SpringBootServletInitializer {
+	private static final Logger LOGGER = LoggerFactory.getLogger(AwsGitCiCdAppApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(AwsGitCiCdAppApplication.class, args);
+	}
+	@PostConstruct
+	public void postConstruct(){
+		LOGGER.info("my-first-app Post construct.");
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AwsGitCiCdAppApplication.class);
 	}
 }
